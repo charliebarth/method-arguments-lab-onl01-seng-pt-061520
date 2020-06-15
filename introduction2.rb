b203e343d7dab77e4c48358cfb4c741cf1200685
Charlie Barth <charliepbarth@gmail.com>
2020-06-15 18:49:47 +0000

    Automatically backed up by Learn

---
+++ introduction2.rb
@@ -0,0 +1,11 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introdution_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction("Josh")
+
+introdution_with_language("Dan", "Ember")
